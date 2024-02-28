725d10e9ac0e337dac59c76103de9a8b62269fa6
Estelle KOFFI <j.estellekoffi@gmail.com>
2024-02-28 23:50:29 +0000

    petite modif de l'interface crossword

--- src/estelle/controllers/GridController.java
+++ src/estelle/controllers/GridController.java
@@ -7,11 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GridController extends Application{
-	
-	public static void main(String[] args){
-		launch(args);
 
-	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
